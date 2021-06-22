90ea8e9d5112464073244e41f7ca8dbf3e71d63d
wwbeamon <wwbeamon@miners.utep.edu>
2021-06-22 15:05:03 -0700

    create graphs from seperated info with table of combinations colorspace x Index

--- Phenoanalyzer_output_graphs.R
+++ Phenoanalyzer_output_graphs.R
@@ -28,5 +28,49 @@ cam3.pheno.2010.long<- cam3.pheno.2010.long %>%
   separate(img, c("img1",NA), sep = ".jpg", remove = FALSE) %>%
   separate(img1,c(NA,"camera","date.info"), sep = "_", extra = "merge", fill = "left") %>%
   mutate(datetime= ymd_hms(date.info),
-         date=as.Date(datetime)) %>%
-  separate(index,c("colorspace","ROI","Index"), sep = "\\.", extra = "merge", fill = "left", remove =FALSE)
+         date=as.Date(datetime),
+         camera=as.integer(camera)) %>%
+  separate(index,c("colorspace","ROI","Index"), sep = "\\.", extra = "merge", fill = "left", remove =FALSE) %>%
+    mutate(across(c(colorspace, ROI, Index),factor))
+
+
+#view catagories in colorspace, ROI, Index
+levels(cam3.pheno.2010.long$colorspace)
+levels(cam3.pheno.2010.long$Index)
+
+#create table of colorspace & Index
+table1<- unique(cam3.pheno.2010.long[c("colorspace","Index" )])
+
+#graph 1 index & 1 ROI
+cam3.pheno.2010.long %>%
+  filter(Index =="redDN"& ROI %in% c("ROI_01", "ROI_06")) %>%
+  ggplot(.,aes(date,value, color=ROI))+
+  geom_line()
+
+
+#graph 1 index for all ROIs
+
+cam3.pheno.2010.long %>%
+  filter(Index =="redDN") %>%
+  ggplot(.,aes(date,value, color=ROI))+
+  geom_line()
+
+
+#graph All index in one colorspace for all ROIs
+
+cam3.pheno.2010.long %>%
+  filter(Index %in% c("redDN","greenDN","blueDN")) %>%
+  ggplot(.,aes(date,value, color=ROI))+
+  geom_line()+
+  facet_grid(Index~.)
+
+#graph All index in one colorspace for all ROIs
+
+cam3.pheno.2010.long %>%
+  filter(colorspace %in% c("RGB")) %>%
+  ggplot(.,aes(date,value, color=ROI))+
+  geom_line()+
+  facet_grid(Index~.,scales = "free_y")
+
+
+
